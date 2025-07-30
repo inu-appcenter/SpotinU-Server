f8f74250255c1de14cad6ec20ff9690689893db0
frozzun <eonjun0783@naver.com>
2025-07-31 00:23:03 +0900

    initial commit

---
+++ spotinu/src/test/java/kr/inuappcenter/spotinu/SpotinuApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.inuappcenter.spotinu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotinuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
